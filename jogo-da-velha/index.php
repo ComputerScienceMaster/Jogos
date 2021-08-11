f9d03ddbec02cbd6d70a3ba2cd09497a4b210be0
Vinicius dos Santos <vinistos@gmail.com>
2021-08-11 10:08:21 -0300

    Update index.php

--- jogo-da-velha/index.php
+++ jogo-da-velha/index.php
@@ -1,9 +1,5 @@
 <html>
 
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-</head>
-
 <div style="font-size:50px; text-align:center; margin-top:50px">
     <?php
     session_start();
